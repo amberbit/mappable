5a1bdbada490b7ba9b9ea4dd1e2046cbb6a7993f
Hubert Łępicki <hubert.lepicki@amberbit.com>
2017-02-18 18:02:57 +0100

    initial commit

---
+++ lib/mappable.ex
@@ -0,0 +1,18 @@
+defmodule Mappable do
+  @moduledoc """
+  Documentation for Mappable.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mappable.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mappable_test.exs
@@ -0,0 +1,8 @@
+defmodule MappableTest do
+  use ExUnit.Case
+  doctest Mappable
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
