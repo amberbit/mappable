1131cc616ba2e958ae51e257a4f2aaff68a29f7a
Hubert Łępicki <hubert.lepicki@amberbit.com>
2017-02-19 11:07:36 +0100

    to_map / to_struct added

--- lib/mappable.ex
+++ lib/mappable.ex
@@ -1,18 +1,32 @@
 defmodule Mappable do
-  @moduledoc """
-  Documentation for Mappable.
-  """
+  def to_map(struct_or_map, options) do
+    case Map.has_key?(struct_or_map, :__struct__) do
+      true -> Map.from_struct(struct_or_map)
+      false -> struct_or_map
+    end |> Enum.reduce(%{}, fn ({k, v}, acc) -> Map.put(acc, convert_key(k, options[:keys]), v) end)
+  end
 
-  @doc """
-  Hello world.
+  defp convert_key(k, :atoms) when is_atom(k) do
+    k
+  end
 
-  ## Examples
+  defp convert_key(k, :atoms) when is_binary(k) do
+    String.to_atom(k)
+  end
 
-      iex> Mappable.hello
-      :world
+  defp convert_key(k, :strings) do
+    "#{k}"
+  end
 
-  """
-  def hello do
-    :world
+  def to_struct(map, struct_module) when is_atom(struct_module) do
+    struct = struct(struct_module)
+
+    Enum.reduce Map.to_list(struct), struct, fn {k, _}, acc ->
+      case Map.fetch(to_map(map, keys: :strings), convert_key(k, :strings)) do
+        {:ok, v} -> %{acc | k => v}
+        :error -> acc
+      end
+    end
   end
 end
+
--- test/mappable_test.exs
+++ test/mappable_test.exs
@@ -1,8 +1,80 @@
+defmodule TestStruct do
+  defstruct foo: nil
+end
+
+defmodule TestStruct2 do
+  defstruct bar: nil, foo: nil
+end
+
 defmodule MappableTest do
   use ExUnit.Case
-  doctest Mappable
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "to_map(something, keys: :strings)" do
+    test "converts to Map with String keys by default" do
+      original = %{:foo => :bar}
+      converted = Mappable.to_map original, keys: :strings
+
+      assert converted == %{"foo" => :bar}
+    end
+
+    test "keeps Map with String keys intact" do
+      original = %{"foo" => :bar}
+      converted = Mappable.to_map original, keys: :strings
+
+      assert original == converted
+    end
+
+    test "converts Struct to Map" do
+      original = %TestStruct{:foo => :bar}
+      converted = Mappable.to_map original, keys: :strings
+
+      assert converted == %{"foo" => :bar}
+    end
+  end
+
+  describe "to_map(something, keys: :atoms)" do
+    test "converts to Map with Atom keys if passed such option" do
+      original = %{"foo" => :bar}
+      converted = Mappable.to_map original, keys: :atoms
+
+      assert converted == %{:foo => :bar}
+    end
+
+    test "keeps Map with Aom keys intact" do
+      original = %{:foo => :bar}
+      converted = Mappable.to_map original, keys: :atoms
+
+      assert converted == original
+    end
+  end
+
+  describe "to_struct(something, struct_type)" do
+    test "converts Map with String keys to struct" do
+      original = %{"foo" => :bar}
+      converted = Mappable.to_struct original, TestStruct
+
+      assert converted == %TestStruct{foo: :bar}
+    end
+
+    test "ignores keys that are not present in destination struct" do
+      original = %{"foo" => :bar, "ignore" => :me}
+      converted = Mappable.to_struct original, TestStruct
+
+      assert converted == %TestStruct{foo: :bar}
+    end
+
+    test "converts Map with Atom keys to struct" do
+      original = %{:foo => :bar}
+      converted = Mappable.to_struct original, TestStruct
+
+      assert converted == %TestStruct{foo: :bar}
+    end
+
+    test "converts other struct to destination struct" do
+      original = %TestStruct2{:bar => :foo, :foo => :bar}
+      converted = Mappable.to_struct original, TestStruct
+
+      assert converted == %TestStruct{foo: :bar}
+    end
   end
 end
